61089761d55ad8c3be214a726c7a6a7318187956
coouir <166575866+coouir@users.noreply.github.com>
2024-04-13 18:50:55 +0900

    Title: a 부터 b 까지, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240413/a 부터 b 까지/a-to-b.py
+++ 240413/a 부터 b 까지/a-to-b.py
@@ -3,9 +3,11 @@ a = int(a); b = int(b)
 print(a, end=" ")
 
 for i in range(a, b+1):
-    if a%2 == 1:
-        a *= 2
-    else:
-        a *= 3
+    
+    if b > a:
+        if a%2 != 0:
+            a *= 2
+        else:
+            a += 3
 
-    print(a, end=" ")
+        print(a, end=" ")
