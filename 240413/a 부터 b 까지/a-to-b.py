77d230de8324ae0da266f7f641ab7e02b696bf16
coouir <166575866+coouir@users.noreply.github.com>
2024-04-13 18:55:14 +0900

    Title: a 부터 b 까지, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240413/a 부터 b 까지/a-to-b.py
+++ 240413/a 부터 b 까지/a-to-b.py
@@ -1,13 +1,8 @@
 a, b = input().split()
 a = int(a); b = int(b)
-print(a, end=" ")
-
-for i in range(a, b+1):
-    
-    if a%2 != 0:
+while a <= b:
+    print(a, end=" ")
+    if a%2 == 1:
         a *= 2
     else:
         a += 3
-    
-    if b >= a:
-        print(a, end=" ")
