d1eb2377485930b451d8663f7d799915ec9f8539
coouir <166575866+coouir@users.noreply.github.com>
2024-04-16 16:28:22 +0900

    Title: 문자열 범위 출력하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/문자열 범위 출력하기/print-string-in-range.py
+++ 240416/문자열 범위 출력하기/print-string-in-range.py
@@ -1,10 +1,3 @@
 a = input()
-check = False
-
-for i in range(len(a)):
-    if a[i] == " ":
-        check = True
-
-if check == True:
-    for i in range(2, 10):
+for i in range(2, 10):
         print(a[i], end="")
