abd5c84d691837758694396c484da5f770befdf6
coouir <166575866+coouir@users.noreply.github.com>
2024-05-09 12:32:09 +0900

    Title: 체크 다이아몬드, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(n):
+    for a in range(n-i):
+        print(' ', end='')
+
+    for b in range(i+1):
+        print('*', end=' ')
+    print()
+
+for i in range(n-1):
+    for c in range(i+2):
+        print(' ', end='')
+    
+    for d in range(n-1-i):
+        print('*', end=' ')
+    print()
