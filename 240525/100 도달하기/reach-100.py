4ad4c612242608731c8aab806b7f5fc95b2339b4
coouir <166575866+coouir@users.noreply.github.com>
2024-05-25 00:32:53 +0900

    Title: 100 도달하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240525/100 도달하기/reach-100.py
+++ 240525/100 도달하기/reach-100.py
@@ -3,11 +3,9 @@ n = int(input())
 arr = []; arr.append(1); arr.append(n)
 
 while True:
-    if arr[-2]+arr[-1] > 100:
-        arr.append(arr[-2] + arr[-1])
+    arr.append(arr[-2] + arr[-1])
+    if arr[-1] > 100:
         break
-    else:
-        arr.append(arr[-2] + arr[-1])
 
 for i in arr:
     print(i, end=' ')
