567e2293ce273685e9c269611851f0df8b8bd7e1
coouir <166575866+coouir@users.noreply.github.com>
2024-06-02 17:31:38 +0900

    Title: 두 정수에 대한 연산값, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240602/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,11 @@
+def f(a, b):
+    if a > b:
+        a += 25; b *= 2
+    else:
+        a *= 2; b += 25
+    
+    return a, b
+
+a, b = tuple(map(int, input().split()))
+a, b = f(a, b)
+print(a, b)
