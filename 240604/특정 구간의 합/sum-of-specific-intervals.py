2752892d08aaa371b7d3ec9d1096ffd168b3d242
coouir <166575866+coouir@users.noreply.github.com>
2024-06-04 13:28:34 +0900

    Title: 특정 구간의 합, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240604/특정 구간의 합/sum-of-specific-intervals.py
+++ 240604/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,11 +1,12 @@
-def f(m):
-    for i in range(m):
-        a1, a2 = tuple(map(int, input().split()))
-        s = 0
-        for j in range(a1-1,a2):
-            s += list_[j]
-        print(s)
+def s(a1, a2):
+    result = 0
+    for j in range(a1-1, a2):
+        result += list_[j]
+    return result
 
 n, m = tuple(map(int, input().split()))
 list_ = list(map(int, input().split()))
-f(m)
+
+for i in range(m):
+    a1, a2 = tuple(map(int, input().split()))
+    print(s(a1, a2))
