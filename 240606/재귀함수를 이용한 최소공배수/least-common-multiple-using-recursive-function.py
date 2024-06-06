5c5f8f63b3a05bae01e0bd1e83b41cd546ee5b83
coouir <166575866+coouir@users.noreply.github.com>
2024-06-06 12:24:49 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240606/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240606/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,16 +1,17 @@
 def get(a, b): # 두 수의 최소공배수
     num = 1
-    for i in range(2, min(a, b)):
+    for i in range(2, min(a, b)+1):
         if a%i == 0 and b%i == 0:
            num = i
     return (a*b)//num
+
+def f(num, a):
+    if num == -1:
+        return a
+    a = get(list_[num],a)   
+    return f(num-1, a)
 n = int(input())
 list_ = list(map(int, input().split()))
 # 최소공배수 a, b> (a*b)/(최대공약수) 
 # 최소공배수, 다음수
-a = get(list_[0], list_[1])
-for i in range(len(list_)-3):
-    a = get(a ,list_[i+2])
-    #a = get(a, list_[2])
-    
-print(a)
+print(f(n-1, 1))
