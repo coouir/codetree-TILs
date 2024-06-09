4139a600b106dbdaba22e4365eebbcd29fbfd1c6
coouir <166575866+coouir@users.noreply.github.com>
2024-06-10 02:26:49 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240610/배열 놀이/play-with-array.py
@@ -0,0 +1,12 @@
+n, q = tuple(map(int, input().split()))
+nList = list(map(int, input().split()))
+
+for i in range(q):
+    qList = list(map(int, input().split()))
+    if qList[0] == 1:
+        print(nList[qList[1]-1])
+    elif qList[0] == 2:
+        print(nList.index(qList[1])+1)
+    elif qList[0] == 3:
+        for j in range(qList[1]-1, qList[2]):
+            print(nList[j], end=' ')
