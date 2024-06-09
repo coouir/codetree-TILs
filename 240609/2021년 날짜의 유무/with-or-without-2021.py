eef2f71ee2b15d0a2e811e28c2e7a528d3b7500d
coouir <166575866+coouir@users.noreply.github.com>
2024-06-09 11:28:37 +0900

    Title: 2021년 날짜의 유무, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240609/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,13 @@
+def f(m, d):
+    m_ = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if 1 <= m <= 12 and (m_[m-1] > d):
+        return True
+    return False
+
+        
+
+m, d = tuple(map(int, input().split()))
+if f(m, d):
+    print('Yes')
+else:
+    print('No')
