376d2bbb8e3085e44a55eb6fd88d4d036281de7d
coouir <166575866+coouir@users.noreply.github.com>
2024-06-09 11:28:52 +0900

    Title: 2021년 날짜의 유무, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 240609/2021년 날짜의 유무/with-or-without-2021.py
+++ 240609/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,6 +1,6 @@
 def f(m, d):
     m_ = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if 1 <= m <= 12 and (m_[m-1] > d):
+    if 1 <= m <= 12 and (m_[m-1] >= d):
         return True
     return False
 
