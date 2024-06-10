5161766154e932fc58356d65c8ae728391ea7398
coouir <166575866+coouir@users.noreply.github.com>
2024-06-11 00:37:08 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240611/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240611/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,10 +1,10 @@
 n = int(input())
 nList = list(map(int, input().split()))
 
-cnt = 1
+cnt = 0
 for i in range(len(nList)):
+    if nList[i] == 2:
+        cnt += 1
     if cnt == 3:
         print(i+1)
         break
-    if nList[i] == 2:
-        cnt += 1
