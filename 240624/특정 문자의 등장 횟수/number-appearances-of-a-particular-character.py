d10a830ff6c18a58a37ac73804365b2dc44c9aa8
coouir <166575866+coouir@users.noreply.github.com>
2024-06-24 18:17:03 +0900

    Title: 특정 문자의 등장 횟수, Time: 233ms, Memory: 24MB, Status: Passed - CodeTree

--- 240624/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240624/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,7 +1,11 @@
 s = input()
 
-if 'ee' in s:
-    print(s.count('ee'), end=' ')
+eec = 0; ebc = 0
 
-if 'eb' in s:
-    print(s.count('eb'))
+for i in range(len(s)-1):
+    if s[i]+s[i+1] == 'ee':
+        eec +=1
+    if s[i]+s[i+1] == 'eb':
+        ebc += 1
+
+print(eec, ebc)
