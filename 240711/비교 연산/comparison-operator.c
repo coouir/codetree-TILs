46e614225ccbd9ddd54e6aff081c5c9413921bcc
coouir <166575866+coouir@users.noreply.github.com>
2024-07-11 14:50:03 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/비교 연산/comparison-operator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if (a >= b) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    if (a > b) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    if (b >= a) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    if (b > a) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    if (a == b) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    if (a != b) {
+        printf("1");
+    } else {
+        printf("0");
+    }
+    return 0;
+}
