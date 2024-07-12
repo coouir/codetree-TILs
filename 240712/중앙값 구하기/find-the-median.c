d2b631747164a4cff68df1c3ee17939b7afc7ab4
coouir <166575866+coouir@users.noreply.github.com>
2024-07-12 14:15:38 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240712/중앙값 구하기/find-the-median.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if ((a > b && b > c) || (c > b && b > a)) {
+        printf("%d", b);
+    } else if ((b > a && a > c) || (c > a && a > b)) {
+        printf("%d", a);
+    } else {
+        printf("%d", c);
+    }
+    return 0;
+}
