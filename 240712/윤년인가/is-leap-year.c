b38bd9c3bb008c2580e6d09b5040d5d3f5e69a3b
coouir <166575866+coouir@users.noreply.github.com>
2024-07-12 13:59:28 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/윤년인가/is-leap-year.c
+++ 240712/윤년인가/is-leap-year.c
@@ -4,14 +4,21 @@ int main() {
     int year;
     scanf("%d", &year);
 
-    if (year%4 == 0) {
-        if (year%100 == 0 && year%400 != 0) {
-            printf("false");
-        } else {
-            printf("true");
-        }
+    //if (year%4 == 0) {
+    //    if (year%100 == 0 && year%400 != 0) {
+    //        printf("false");
+    //    } else {
+    //        printf("true");
+    //    }
+    //} else {
+    //    printf("false");
+    //}
+
+    if (year%4 == 0 && !(year%100 == 0 && year%400 != 0)) {
+        printf("true");
     } else {
         printf("false");
     }
+
     return 0;
 }
