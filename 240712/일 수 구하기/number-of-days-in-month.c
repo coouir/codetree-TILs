b659b26b3e4964101e541f6f5355afc41b6f390e
coouir <166575866+coouir@users.noreply.github.com>
2024-07-12 14:04:21 +0900

    Title: 일 수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240712/일 수 구하기/number-of-days-in-month.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 8) {
+        if (n%2 == 1) {
+            printf("31");
+        } else if (n == 2) {
+            printf("28");
+        } else {
+            printf("30");
+        } 
+    } else {
+        if (n%2 == 0) {
+            printf("31");
+        } else {
+            printf("30");
+        }
+    }
+    return 0;
+}
