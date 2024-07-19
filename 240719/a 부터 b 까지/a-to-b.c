e5702e309f6e6e711b592b2758d9c6da1596096e
coouir <166575866+coouir@users.noreply.github.com>
2024-07-19 19:07:45 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b; scanf("%d %d", &a, &b);
+    while (a <= b) {
+        printf("%d ", a);
+
+        if (a%2 == 1) {
+            a *= 2;
+        } else {
+            a += 3;
+        }
+    }
+    return 0;
+}
