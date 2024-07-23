707ee36cf8f6d90b839d62eee24c758eb39f3c87
coouir <166575866+coouir@users.noreply.github.com>
2024-07-23 16:23:13 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/완전수 판별기/perfect-number-discriminator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n; scanf("%d", &n);
+    int sum = 0;
+
+    for (int i=1; i < n; i++) {
+        if (n%i == 0) {
+            sum += i;
+        }
+    }
+    if (n == sum) {
+        printf("P");
+    } else {
+        printf("N");
+    }
+    return 0;
+}
