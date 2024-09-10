973eb5cf7a148f4f46be86d3763b6e365db4dad5
coouir <166575866+coouir@users.noreply.github.com>
2024-09-10 18:40:49 +0900

    Title: 모이자, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/모이자/gather.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+m = 10000
+for i in range(n):
+    s = 0
+    for j in range(n):
+        if i != j:
+            s += abs(i-j)*arr[j]
+    m = min(m, s)
+print(m)
