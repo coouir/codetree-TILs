1507c2d9fe4382b5bcb30a6657302762ca064f31
coouir <166575866+coouir@users.noreply.github.com>
2024-09-13 17:41:32 +0900

    Title: 체크판위에서 2, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240913/체크판위에서 2/on-the-checkboard-2.py
+++ 240913/체크판위에서 2/on-the-checkboard-2.py
@@ -3,20 +3,20 @@ arr = []
 for i in range(R):
     arr.append(list(input().split()))
 
-n = arr[0][0]
+now = arr[0][0]
 cnt = 0
 
 for i in range(1, R):
     for j in range(1, C):
-        if arr[i][j] != n:
-            n = arr[i][j]
+        if arr[i][j] != now:
+            a = arr[i][j]
             for k in range(i+1, R):
                 for l in range(j+1, C):
-                    if arr[k][l] != n:
-                        n = arr[k][l]
+                    if arr[k][l] != a:
+                        b = arr[k][l]
                         for m in range(k+1, R):
                             for n in range(l+1, C):
-                                if arr[m][n] != n and m == R-1 and n == C-1:
+                                if arr[m][n] != b and m == R-1 and n == C-1:
                                     cnt += 1
 
 print(cnt)
