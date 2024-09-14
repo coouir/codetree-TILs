55b365debcda75058c9dd8a7c07861016bc81fbf
coouir <166575866+coouir@users.noreply.github.com>
2024-09-14 13:41:17 +0900

    Title: 씨 오 더블유 2, Time: 163ms, Memory: 72MB, Status: Passed - CodeTree

---
+++ 240914/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,12 @@
+N = int(input())
+S = input()
+
+cnt = 0
+for i in range(N):
+    if S[i] == 'C':
+        for j in range(i+1, N):
+            if S[j] == 'O':
+                for k in range(j+1, N):
+                    if S[k] == 'W':
+                        cnt += 1
+print(cnt)
