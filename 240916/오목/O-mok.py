01469300c60fc32c751773f010ac9709fe454d7d
coouir <166575866+coouir@users.noreply.github.com>
2024-09-16 00:11:47 +0900

    Title: 오목, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240916/오목/O-mok.py
+++ 240916/오목/O-mok.py
@@ -1,13 +1,13 @@
-arr = [list(map(int, input().split())) for i in range(19)]
+arr = [list(input().split()) for i in range(19)]
 
 # 1
 for i in range(19):
     for j in range(15):
-        if arr[i][j:j+5] == [1, 1, 1, 1, 1]:
+        if arr[i][j:j+5] == ["1", "1", "1", "1", "1"]:
             print(1)
             print(i+1, j+3)
             exit()
-        elif arr[i][j:j+5] == [2, 2, 2, 2, 2]:
+        elif arr[i][j:j+5] == ["2", "2", "2", "2", "2"]:
             print(2)
             print(i+1, j+3)
             exit()
