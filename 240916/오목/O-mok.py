c3fd2d1d26bbd66a13156b93d7e12777707ba171
coouir <166575866+coouir@users.noreply.github.com>
2024-09-16 00:06:26 +0900

    Title: 오목, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240916/오목/O-mok.py
@@ -0,0 +1,45 @@
+arr = [list(map(int, input().split())) for i in range(19)]
+
+# 1
+for i in range(19):
+    for j in range(15):
+        if arr[i][j:j+5] == [1, 1, 1, 1, 1]:
+            print(1)
+            print(i+1, j+3)
+            exit()
+        elif arr[i][j:j+5] == [2, 2, 2, 2, 2]:
+            print(2)
+            print(i+1, j+3)
+            exit()
+
+# 2
+for i in range(15):
+    for j in range(19):
+        if arr[i][j]+arr[i+1][j]+arr[i+2][j]+arr[i+3][j]+arr[i+4][j] == "11111":
+            print(1)
+            print(i+3, j+1)
+            exit()
+        if arr[i][j]+arr[i+1][j]+arr[i+2][j]+arr[i+3][j]+arr[i+4][j] == "22222":
+            print(2)
+            print(i+3, j+1)
+            exit()
+
+# 3
+for i in range(15):
+    for j in range(15):
+        if arr[i][j]+arr[i+1][j+1]+arr[i+2][j+2]+arr[i+3][j+3]+arr[i+4][j+4] == "11111":
+            print(1)
+            print(i+3, j+3)
+            exit()
+        if arr[i][j]+arr[i+1][j+1]+arr[i+2][j+2]+arr[i+3][j+3]+arr[i+4][j+4] == "22222":
+            print(2)
+            print(i+3, j+3)
+            exit()
+
+# 4
+for i in range(15):
+    for j in range(4, 19):
+        if arr[i][j]+arr[i+1][j-1]+arr[i+2][j-2]+arr[i+3][j-3]+arr[i+4][j-4] == "11111":
+            print(1)
+            print(i+3, j-1)
+            exit()
