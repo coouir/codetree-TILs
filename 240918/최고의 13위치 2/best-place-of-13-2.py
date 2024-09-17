30f86a1e5837bc091a2051d869050946e984a76c
coouir <166575866+coouir@users.noreply.github.com>
2024-09-18 00:20:04 +0900

    Title: 최고의 13위치 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240918/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,20 @@
+N = int(input())
+arr = [list(map(int, input().split())) for i in range(N)]
+
+M = 0
+I, J = 0, 0
+for i in range(N):
+    for j in range(N-2):
+        x = sum(arr[i][j:j+3])
+        if M < x:
+            M = x
+            I, J = i, j
+
+m = 0
+for i in range(N):
+    for j in range(N-2):
+        if i == I and (j == J or j == J+1 or j == J+2):
+            continue
+        m = max(m, sum(arr[i][j:j+3]))
+
+print(M + m)
