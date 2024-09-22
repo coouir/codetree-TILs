831f24a0915ae90d495d891ebf2170de9600ea12
coouir <166575866+coouir@users.noreply.github.com>
2024-09-22 14:00:58 +0900

    Title: DateTime to DateTime, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240922/DateTime to DateTime/datetime-to-datetime.py
+++ 240922/DateTime to DateTime/datetime-to-datetime.py
@@ -1,12 +1,10 @@
 a, b, c = map(int, input().split())
 d, h, m = 11, 11, 11
 
-if a == 11:
-    if b == 11:
-        if c < 11:
-            print(-1)
-    elif b < 11:
-        print(-1)
+if a == 11 and b == 11 and c < 11:
+    print(-1)
+elif a == 11 and b < 11:
+    print(-1)
 else:
     cnt = 0
     while True:
@@ -21,5 +19,4 @@ else:
         if h == 24:
             d += 1
             h = 0
-
-print(cnt)
+    print(cnt)
