bd0d36d82ea75e1da8a8a879a5b1da9156e7fac5
coouir <166575866+coouir@users.noreply.github.com>
2024-09-22 13:57:34 +0900

    Title: DateTime to DateTime, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240922/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,25 @@
+a, b, c = map(int, input().split())
+d, h, m = 11, 11, 11
+
+if a == 11:
+    if b == 11:
+        if c < 11:
+            print(-1)
+    elif b < 11:
+        print(-1)
+else:
+    cnt = 0
+    while True:
+        if d == a and h == b and m == c:
+            break
+        m += 1
+        cnt += 1
+        if m == 60:
+            h += 1
+            m = 0
+        
+        if h == 24:
+            d += 1
+            h = 0
+
+print(cnt)
