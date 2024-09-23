c3802891db3d625c8baadb1ccd6bf474ea8e2580
coouir <166575866+coouir@users.noreply.github.com>
2024-09-23 19:29:33 +0900

    Title: 그 요일은, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/그 요일은/the-day-of-the-day.py
+++ 240923/그 요일은/the-day-of-the-day.py
@@ -6,5 +6,5 @@ def f(m, d):
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 arrM = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-d = f(m2, d2) - f(m1, d1) 
+d = f(m2, d2) - f(m1, d1)
 print(d//7 + 1)
