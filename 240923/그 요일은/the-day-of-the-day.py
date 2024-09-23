a25f4088b8bd3d04de614c9383bdb2ce10a3c98c
coouir <166575866+coouir@users.noreply.github.com>
2024-09-23 19:38:43 +0900

    Title: 그 요일은, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/그 요일은/the-day-of-the-day.py
+++ 240923/그 요일은/the-day-of-the-day.py
@@ -1,10 +1,33 @@
-def f(m, d):
+arrM = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def days_from_start_of_year(m, d):
+    total_days = 0
     for i in range(1, m):
-        d += arrM[i]
-    return d
+        total_days += arrM[i]
+    total_days += d
+    return total_days
 
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
-arrM = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-d = f(m2, d2) - f(m1, d1)
-print(d//7 + 1)
+
+day_str_to_num = {
+    "Mon": 0, "Tue": 1, "Wed": 2, "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6
+}
+target_weekday = day_str_to_num[A]
+
+start_weekday = 0
+
+start_total_days = days_from_start_of_year(m1, d1)
+end_total_days = days_from_start_of_year(m2, d2)
+
+days_between = end_total_days - start_total_days
+
+start_date_weekday = (start_weekday + (start_total_days - 1)) % 7
+
+count = 0
+for i in range(days_between + 1):
+    if (start_date_weekday + i) % 7 == target_weekday:
+        count += 1
+
+# 결과 출력
+print(count)
