f7a1ed1d4fa6c255025fc718834d4ea2a0cef9b1
coouir <166575866+coouir@users.noreply.github.com>
2024-09-25 02:54:53 +0900

    Title: 진수 to 진수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,15 @@
+a, b = map(int, input().split())
+binary = list(map(int, input()))
+
+n = 0
+for i in range(len(binary)):
+    n = n*a + binary[i]
+
+arr = []
+while True:
+    if n < b:
+        arr.append(n)
+        break
+    arr.append(n%2)
+    n //= 2
+print(''.join(map(str, arr[::-1])))
