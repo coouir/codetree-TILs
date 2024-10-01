4a2e831be6e46505f1f863aaed979e3c142368a1
coouir <166575866+coouir@users.noreply.github.com>
2024-10-01 20:25:32 +0900

    Title: 왔다 갔던 구역 2, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241001/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,6 +1,6 @@
 n = int(input())
-arr = [0]*201
-now = 100
+arr = [0]*2001
+now = 1001
 
 for i in range(n):
     x, D = input().split()
@@ -12,7 +12,6 @@ for i in range(n):
         for j in range(int(x)):
             now += 1
             arr[now] += 1
-
 cnt = 0
 for i in range(len(arr)):
     if arr[i] >= 2:
