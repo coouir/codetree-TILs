8d9541a51c2b30975eaecd01b16a01791d1e1c20
coouir <166575866+coouir@users.noreply.github.com>
2024-10-03 11:42:48 +0900

    Title: 왔다 갔던 구역 2, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 241003/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241003/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,20 +1,19 @@
 arr = [0] * 2001
 now = 1000
 n = int(input())
+
 for i in range(n):
     x, D = input().split()
-    arr[now] += 1
     if D == 'L':
-        for j in range(int(x)-1):
+        for j in range(int(x)):
             now -= 1
             arr[now] += 1
-        now -= 1
     else:
-        for j in range(int(x)-1):
+        arr[now] += 1
+        for j in range(int(x)):
             now += 1
-            arr[now] += 1
-        now += 1
-
+            if j != int(x)-1:
+                arr[now] += 1
 cnt = 0
 for i in range(2001):
     if arr[i] >= 2:
