bf3ad9d627fc7e0a17562f9193ae1b5b9a3e25ed
coouir <166575866+coouir@users.noreply.github.com>
2024-10-04 17:56:04 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 167ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 241004/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 241004/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -3,13 +3,10 @@ x1B, y1B, x2B, y2B = map(int, input().split())
 
 arr = [[0]*2001 for i in range(2001)]
 
-S = (x2A-x1A) * (y2A-y1A)
-
 for i in range(x1A, x2A+1):
     for j in range(y1A, y2A+1):
         arr[i+1000][j+1000] = 1
 
-s = 0
 for i in range(x1B, x2B+1):
     for j in range(y1B, y2B+1):
         if arr[i+1000][j+1000] == 1:
@@ -21,7 +18,10 @@ for i in range(2001):
         if arr[i][j] == 1:
             points.append([i, j])
 
-w = abs(points[-1][0]-points[0][0])
-points.sort(key = lambda s: [s[1]])
-h = abs(points[-1][1]-points[0][1])
-print(w*h)
+if len(points) != 0:
+    w = abs(points[-1][0]-points[0][0])
+    points.sort(key = lambda s: [s[1]])
+    h = abs(points[-1][1]-points[0][1])
+    print(w*h)
+else:
+    print(0)
