848623cda44ef3a3cca9970cd8af2bd18771af29
coouir <166575866+coouir@users.noreply.github.com>
2024-10-07 19:32:43 +0900

    Title: 만나는 그 순간, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241007/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,27 @@
+N, M = map(int, input().split())
+
+arrN = [0]
+arrM = [0]
+
+for i in range(N):
+    d, t = input().split()
+    if d == 'L':
+        for j in range(int(t)):
+            arrN.append(arrN[-1]-1)
+    else:
+        for j in range(int(t)):
+            arrN.append(arrN[-1]+1)
+
+for i in range(M):
+    d, t = input().split()
+    if d == 'L':
+        for j in range(int(t)):
+            arrM.append(arrM[-1]-1)
+    else:
+        for j in range(int(t)):
+            arrM.append(arrM[-1]+1)
+
+for i in range(1, len(arrN)):
+    if arrN[i] == arrM[i]:
+        print(i)
+        break
