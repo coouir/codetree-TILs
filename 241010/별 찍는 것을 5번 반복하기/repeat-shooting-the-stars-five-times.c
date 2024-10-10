1ea9febfbdbe6801aaeaf4d6361cdb2608630dc1
coouir <166575866+coouir@users.noreply.github.com>
2024-10-10 13:26:34 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void prints_star(void) {
+    printf("**********\n");
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    prints_star();
+    prints_star();
+    prints_star();
+    prints_star();
+    prints_star();
+    
+    return 0;
+}
