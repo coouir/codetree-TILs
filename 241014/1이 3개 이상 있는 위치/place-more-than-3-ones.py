66bdbaea84a43f8c01f09c3c8b424f871c3ef31d
coouir <166575866+coouir@users.noreply.github.com>
2024-10-14 18:42:31 +0900

    Title: 1이 3개 이상 있는 위치, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 241014/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 241014/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,4 +1,21 @@
 n = int(input())
-arr = [list(map(int, input())) for i in range(n)]
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
 
-print(arr)
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+def f(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+cnt = 0
+for i in range(n):
+    for j in range(n):
+        c = 0
+        for dx, dy in zip(dxs, dys):
+            nx, ny = i + dx, j + dy
+            if f(nx, ny) and arr[nx][ny] == 1:
+                c += 1
+        if c >= 3:
+            cnt += 1
+print(cnt)
