07a3fa1b2979c53b4ba9358712d22a78a06751bc
coouir <166575866+coouir@users.noreply.github.com>
2024-10-15 19:03:23 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241015/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241015/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -12,14 +12,16 @@ arr.sort(key = lambda x: [x[0]])
 for i in range(T):
     x, y = arr[i][1], arr[i][2]
     if arrN[x] == 1:
-        if cntN[x] < K and arrN[y] == 0:
+        if cntN[x] <= K and arrN[y] == 0:
             arrN[y] = 1
+            cntN[x] += 1
+            cntN[y] += 1
         
     else:
         if arrN[y] == 1:
-            if cntN[y] < K and arrN[x] == 0:
+            if cntN[y] <= K and arrN[x] == 0:
                 arrN[x] = 1
-    cntN[x] += 1
-    cntN[y] += 1
+                cntN[x] += 1
+                cntN[y] += 1
 
 print(''.join(map(str, arrN[1:])))
