2202ed53a6b413e57395f13964577709a2bfe27d
coouir <166575866+coouir@users.noreply.github.com>
2024-10-18 17:30:09 +0900

    Title: 대각선으로 숫자 채우기, Time: 222ms, Memory: 28MB, Status: Passed - CodeTree

--- 241018/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 241018/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -1,20 +1,19 @@
 n, m = map(int, input().split())
-
 arr = [[0]*m for _ in range(n)]
 
-N = 1
-def diagonal(x, y):
-    global N
+number = 1
+def f(x, y):
+    global number
     if not(0 <= x < n and 0 <= y < m):
         return 
-    arr[x][y] = N
-    N += 1
-    return diagonal(x+1, y-1)
+    arr[x][y] = number
+    number += 1
+    return f(x+1, y-1)
 
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 0:
-            diagonal(i, j)
+            f(i, j)
 
 for i in range(n):
     for j in range(m):
