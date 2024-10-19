b0331ea636eb8da7465ee4a396b42edf4acbf4d9
coouir <166575866+coouir@users.noreply.github.com>
2024-10-19 14:42:23 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241019/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,25 @@
+N, K, P, T = map(int, input().split());
+
+arrN = [0]*(N+1)
+arrN[P] = 1
+cntN = [0]*(N+1)
+
+arr = []
+for i in range(T):
+    arr.append(list(map(int, input().split())))
+arr.sort(key = lambda x: [x[0]])
+
+for i in range(T):
+    x, y = arr[i][1], arr[i][2]
+    if arrN[x] == 1:
+        if cntN[x] < K:
+            arrN[y] = 1
+            cntN[x] += 1
+        
+    else:
+        if arrN[y] == 1:
+            if cntN[y] < K: 
+                arrN[x] = 1
+                cntN[y] += 1
+
+print(''.join(map(str, arrN[1:])))
