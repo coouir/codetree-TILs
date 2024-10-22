e7a8b457472bf9f028e77b05ef9f9a778034df86
coouir <166575866+coouir@users.noreply.github.com>
2024-10-22 14:07:55 +0900

    Title: 거울에 레이저 쏘기 2, Time: 147ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 241022/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,56 @@
+N = int(input())
+
+arr = []
+for i in range(N+2):
+    if i == 0 or i == (N+1):
+        arr.append([0]*(N+2))
+    else:
+        arr.append([0]+list(input())+[0])
+
+cnt = 1
+for i in range(1, N+1):
+    arr[0][i] = cnt
+    cnt += 1
+for i in range(1, N+1):
+    arr[i][N+1] = cnt
+    cnt += 1
+for i in range(1, N+1):
+    arr[N+1][N+1-i] = cnt
+    cnt += 1
+for i in range(1, N+1):
+    arr[N+1-i][0] = cnt
+    cnt += 1
+
+def in_range(x, y):
+    return not(0 <= x < (N+2) and 0 <= y < (N+2))
+
+dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
+
+d_x, d_y = 0, 0
+d = 0
+K = int(input())
+for i in range(N+2):
+    for j in range(N+2):
+        if arr[i][j] == K:
+            d_x, d_y = i, j
+            for dir_num in range(4):
+                x, y = i+dxs[dir_num], j+dys[dir_num]
+                if in_range(x, y):
+                    d = 3-dir_num
+
+if d == 2:
+    d = 3
+elif d == 3:
+    d = 2
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0] #동남서북
+
+d_x, d_y = d_x + dxs[d], d_y + dys[d]
+# 0 1 2 3
+while(not(str(arr[d_x][d_y]).isdigit())):
+    if arr[d_x][d_y] == '/':
+        d = (d + 1) % 4
+    else:
+        d = (d + 3) % 4
+    d_x, d_y = d_x + dxs[d], d_y + dys[d]
+
+print(arr[d_x][d_y])
