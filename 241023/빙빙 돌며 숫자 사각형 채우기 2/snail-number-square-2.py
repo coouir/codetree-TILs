ea608bf3efcba91bf6aacf2043bb6dea337b54c0
coouir <166575866+coouir@users.noreply.github.com>
2024-10-23 18:30:47 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241023/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 241023/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -4,7 +4,6 @@ answer = [[0] * m for _ in range(n)]
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m 
 
-
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 x, y = 0, 0
 dir_num = 1
@@ -15,11 +14,11 @@ for i in range(2, n * m + 1):
     
     if not in_range(nx, ny) or answer[nx][ny] != 0:
         dir_num = (dir_num + 3) % 4
+    print(x, y, i)
 
     x, y = x + dxs[dir_num], y + dys[dir_num]
-    answer[x][y] = i
 
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         print(answer[i][j], end = ' ')
     print()
