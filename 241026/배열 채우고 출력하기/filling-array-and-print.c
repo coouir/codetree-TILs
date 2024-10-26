140933543c26fa371b8aeb0c861fc3e020a26bd2
coouir <166575866+coouir@users.noreply.github.com>
2024-10-26 19:18:27 +0900

    Title: 배열 채우고 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/배열 채우고 출력하기/filling-array-and-print.c
+++ 241026/배열 채우고 출력하기/filling-array-and-print.c
@@ -3,8 +3,7 @@
 int main() {
     char arr[10];
     for (int i=0; i<10; i++) {
-        scanf("%c", &arr[i]);
-        getchar();
+        scanf("%c ", &arr[i]);
     }
 
     for (int i=0; i<10; i++) {
