6f492677496acad454b7cc3edd2912158f93538b
coouir <166575866+coouir@users.noreply.github.com>
2024-11-08 15:39:43 +0900

    Title: 개발팀의 능력, Time: 45ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241108/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,22 @@
+import sys
+
+arr = list(map(int, input().split()))
+d = sys.maxsize
+
+for i in range(5):
+    for j in range(i+1, 5):
+
+        for a in range(5):
+            if a == i or a == j:
+                continue
+            
+            x = arr[i] + arr[j]
+            y = arr[a]
+            z = sum(arr) - (x+y)
+            if x != y and y != z:
+                d = min(d, max(x,y,z) - min(x,y,z))
+
+if d == sys.maxsize:
+    print(-1)
+else:
+    print(d)
