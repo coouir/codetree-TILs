11113f66d07e379d8903dc817e79cc1e09e345b9
coouir <166575866+coouir@users.noreply.github.com>
2024-11-11 03:09:03 +0900

    Title: 삼각형 만들기, Time: 58ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241111/삼각형 만들기/create-triangle.py
+++ 241111/삼각형 만들기/create-triangle.py
@@ -9,5 +9,5 @@ for i in range(N):
             x1, y1 = arr[i][0], arr[i][1]
             x2, y2 = arr[j][0], arr[j][1]
             x3, y3 = arr[k][0], arr[k][1]
-            S = max(S, ((x1*y2)+(x2*y3)+(x3*y1))-((x2*y1)+(x3*y2)+(x1*y3)))
+            S = max(S, abs((x1*y2)+(x2*y3)+(x3*y1))-((x2*y1)+(x3*y2)+(x1*y3)))
 print(S)
