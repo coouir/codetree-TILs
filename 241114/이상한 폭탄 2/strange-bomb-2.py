9de518678d1ba828bdc1c0073f5e48b76933ccd1
coouir <166575866+coouir@users.noreply.github.com>
2024-11-14 16:20:13 +0900

    Title: 이상한 폭탄 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241114/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+
+M = -1
+
+for i in range(N):
+    for j in range(i+1, N):
+        if arr[j] == arr[i] and j-i <=3:
+            M = max(M, arr[j])
+
+print(M)
