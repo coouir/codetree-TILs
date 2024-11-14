88b964bcc24bd9a1d8195573ae5b2107bf27e2a1
coouir <166575866+coouir@users.noreply.github.com>
2024-11-14 19:51:09 +0900

    Title: 겹치지 않는 선분 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 241114/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -4,16 +4,28 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 cnt = 0
 
 for i in range(N):
-    x1 = arr[i][0]
-    x2 = arr[i][1]
+    check = True
     for j in range(N):
         if i == j:
             continue
+        x1 = arr[i][0]
+        x2 = arr[i][1]
         x3 = arr[j][0]
         x4 = arr[j][1]
+        
+        if x1 <= x2:
+            if x3 < x1 and x4 < x2:
+                continue
+            elif x1 < x3 and x2 < x4:
+                continue
+        else:
+            if x4 < x2 and x3 < x1:
+                continue
+            elif x2 < x4 and x1 < x3:
+                continue
+        check = False
+        break
+    if check:
+        cnt += 1
 
-        if x1 < x3 < x4 < x2:
-            cnt += 1
-        elif x3 < x1 < x2 < x4:
-            cnt += 1
 print(cnt)
