68179ef775c41d3d7e225455dc6176868dbe9432
coouir <166575866+coouir@users.noreply.github.com>
2024-11-15 20:35:10 +0900

    Title: 스승의 은혜 3, Time: 69ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241115/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,20 @@
+N, B = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+arr = sorted(arr, key = lambda x: [x[0]])
+
+M = 0
+for i in range(N):
+    cntC = arr[i][0]/2
+    cntP = 1
+    for j in range(N):
+        if i == j:
+            continue
+        cntC += (arr[j][0]+arr[j][1])
+        if cntC > B:
+            break
+        else:
+            cntP += 1
+    M = max(M, cntP)
+
+print(M)
