dc85e79301c51727d14cab58e37a6daf07d4a765
coouir <166575866+coouir@users.noreply.github.com>
2024-11-15 20:28:25 +0900

    Title: 스승의 은혜 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241115/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 241115/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -6,12 +6,11 @@ M = 0
 for i in range(N):
     cntC = arr[i]/2
     cntP = 1
-
     for j in range(N):
         if i == j:
             continue
         cntC += arr[j]
-        if cntC > B:
+        if cntC >= B:
             M = max(M, cntP)
             break
         else:
