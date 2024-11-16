5cddf45dc59be8779d9ee18d0df6032da68249c0
coouir <166575866+coouir@users.noreply.github.com>
2024-11-16 17:22:30 +0900

    Title: 상해버린 치즈, Time: 70ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241116/상해버린 치즈/rotten-cheese.py
+++ 241116/상해버린 치즈/rotten-cheese.py
@@ -4,16 +4,18 @@ arrS = [list(map(int, input().split())) for _ in range(S)]
 arrD.sort(key = lambda x: [x[1]])
 
 arr = [0]*51
+arrC = [0]*51
 
 for i in range(D):
     for j in range(S):
-        if arrD[i][0] == arrS[j][0] and arrD[i][2] < arrS[j][1]:
+        if arrD[i][0] == arrS[j][0] and arrD[i][2] < arrS[j][1] and arrC[arrD[i][0]] == 0:
             arr[arrD[i][1]] += 1
+            arrC[arrD[i][0]] = 1
 
 M = 0
 arrP = [0]*51
 for i in range(51):
-    if arr[i] >= S:
+    if arr[i] == S:
         cnt = 0
         for j in range(D):
             if arrD[j][1] == i and arrP[arrD[j][0]] == 0:
