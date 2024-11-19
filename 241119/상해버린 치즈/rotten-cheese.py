c19f39954f19ac75e842cf242f8834f67c749a28
coouir <166575866+coouir@users.noreply.github.com>
2024-11-19 17:17:28 +0900

    Title: 상해버린 치즈, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241119/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,26 @@
+N, M, D, S = map(int, input().split())
+arrD = [list(map(int, input().split())) for _ in range(D)]
+arrS = [list(map(int, input().split())) for _ in range(S)]
+arrD.sort(key = lambda x: [x[1], x[2]])
+
+Max = 0
+
+for i in range(1, M+1):
+    arrP = [0]*51
+    for j in range(D):
+        p, m, t = arrD[j]
+        if i == m:
+            for k in range(S):
+                P, T = arrS[k]
+                if p == P and t < T and arrP[p] == 0:
+                    arrP[p] = 1
+
+    if sum(arrP) == S:
+        cnt = 0
+        for j in range(D):
+            p, m, t = arrD[j]
+            if i == m:
+                cnt += 1
+        Max = max(Max, cnt)
+
+print(Max)
