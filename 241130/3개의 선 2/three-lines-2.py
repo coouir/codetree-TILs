b64ff04fbd0312739a0389b21d8619b053329cf7
coouir <166575866+coouir@users.noreply.github.com>
2024-11-30 13:00:19 +0900

    Title: 3개의 선 2, Time: 58ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241130/3개의 선 2/three-lines-2.py
+++ 241130/3개의 선 2/three-lines-2.py
@@ -1,26 +1,25 @@
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-for i in range(N):
-    x, y = arr[i]
-    arrJ = []
-    for j in range(N):
-        if i == j:
-            continue
-        xj, yj = arr[j]
-        if x != xj and y != yj:
-            arrJ.append([xj, yj])
-    
-    xJ, yJ = arrJ[0]
-    checkX = 1
-    checkY = 1
-    for j in range(1, len(arrJ)):
-        if xJ != arrJ[j][0]:
-            checkX = 0
-        if yJ != arrJ[j][1]:
-            checkY = 0
-    if checkX == 1 or checkY == 1:
-        print(1)
-        exit()
+for i in range(0, 11):
+    for j in range(0, 11):
+        x, y = i, j
+        
+        arrN = []
+        for n in range(N):
+            xN, yN = arr[n]
+            if x != xN and y != yN:
+                arrN.append([xN, yN])
 
+        xM, yM = arrN[0]
+        checkX = 1
+        checkY = 1
+        for m in range(1, len(arrN)):
+            if xM != arrN[m][0]:
+                checkX = 0
+            if yM != arrN[m][1]:
+                checkY = 0
+        if checkX == 1 or checkY == 1:
+            print(1)
+            exit()
 print(0)
