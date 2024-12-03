1a7909f4b05cad632e09211fda0eb1501666acec
coouir <166575866+coouir@users.noreply.github.com>
2024-12-03 18:30:05 +0900

    Title: 팀으로 하는 틱택토 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241203/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,27 @@
+arr = [list(map(int, input())) for _ in range(3)]
+
+cnt = 0
+for i in range(3):
+    if (arr[i][0] == arr[i][1] and arr[i][0] != arr[i][2]):
+        cnt += 1
+    elif (arr[i][0] != arr[i][1] and arr[i][0] == arr[i][2]):
+        cnt += 1
+    elif (arr[i][0] != arr[i][1] and arr[i][1] == arr[i][2]):
+        cnt += 1
+
+for i in range(3):
+    if (arr[0][i] == arr[1][i] and arr[0][i] != arr[2][i]):
+        cnt += 1
+    elif (arr[0][i] != arr[1][i] and arr[0][i] == arr[2][i]):
+        cnt += 1
+    elif (arr[0][i] != arr[1][i] and arr[1][i] == arr[2][i]):
+        cnt += 1
+
+if (arr[0][0] == arr[1][1] and arr[0][0] != arr[2][2]):
+    cnt += 1
+elif (arr[0][0] != arr[1][1] and arr[0][0] == arr[2][2]):
+    cnt += 1
+elif (arr[0][0] != arr[1][1] and arr[1][1] == arr[2][2]):
+    cnt += 1
+    
+print(cnt)
