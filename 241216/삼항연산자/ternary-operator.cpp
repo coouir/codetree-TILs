03c90a989bfa0022f359c157ab68e2b96b3ebb28
coouir <166575866+coouir@users.noreply.github.com>
2024-12-16 16:26:18 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int score;
+    cin >> score;
+
+    if (score == 100) {
+        cout << "pass";
+    } else {
+        cout << "failure";
+    }
+    return 0;
+}
