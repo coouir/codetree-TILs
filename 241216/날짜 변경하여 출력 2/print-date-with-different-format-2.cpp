0b52129cfd0d69b9a4f4a34a8609a99c3c4c2679
coouir <166575866+coouir@users.noreply.github.com>
2024-12-16 14:59:39 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m, d, y;
+    char c;
+
+    cin >> m >> c >> d >> c >> y;
+    cout << y << '.' << m << '.' << d;
+    return 0;
+}
