a8b251ca6330a39c52b86fc27da3b33f9e566ee0
coouir <166575866+coouir@users.noreply.github.com>
2024-12-17 11:06:18 +0900

    Title: 두 수의 짝홀 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241217/두 수의 짝홀 여부/parity-of-two-numbers.cpp
+++ 241217/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -7,9 +7,9 @@ int main() {
     cin >> a >> b;
 
     if (a%2 == 0) {
-        cout << "even";
+        cout << "even" << endl;
     } else {
-        cout << "odd";
+        cout << "odd" << endl;
     }
 
     if (b%2 == 0) {
