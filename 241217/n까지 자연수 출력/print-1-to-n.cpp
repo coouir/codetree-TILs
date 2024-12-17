51dca6a40e1f5493da3f59627769f20c9c96de98
coouir <166575866+coouir@users.noreply.github.com>
2024-12-17 15:52:03 +0900

    Title: n까지 자연수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/n까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i=1, n;
+    cin >> n;
+
+    while (i <= n) {
+        cout << i++ << " ";
+    }
+    return 0;
+}
