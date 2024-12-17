70b5171eab05711c5f0d335afd125655bec1c1f7
coouir <166575866+coouir@users.noreply.github.com>
2024-12-17 13:15:32 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    if (n == 2) {
+        cout << "28";
+    } else if (n <= 7) {
+        if (n%2 == 1) {
+            cout << "31";
+        } else {
+            cout << "30";
+        }
+    } else {
+        if (n%2 == 0) {
+            cout << "31";
+        } else {
+            cout << "30";
+        }
+    }
+    return 0;
+}
