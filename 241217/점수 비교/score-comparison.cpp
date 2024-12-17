c050d7606a4735b831deb4289f3dee2dd548e455
coouir <166575866+coouir@users.noreply.github.com>
2024-12-17 11:23:28 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int aM, aE, bM, bE;
+    cin >> aM >> aE >> bM >> bE;
+
+    cout << (aE > bE && aM > bM);
+    return 0;
+}
