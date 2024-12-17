141e28f4ddad85b0908451a1d817f7867e986d6e
coouir <166575866+coouir@users.noreply.github.com>
2024-12-17 11:54:02 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/장학금/scholarship.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m, f;
+    cin >> m >> f;
+
+    if (m >= 90) {
+        if (f >= 95) {
+            cout << 100000;
+        } else if (f >= 90) {
+            cout << 50000;
+        } else {
+            cout << 0;
+        }
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
