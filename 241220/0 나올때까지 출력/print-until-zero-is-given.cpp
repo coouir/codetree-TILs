75d77fb0e1cbd1eb4164cd1d20cb9fec3c16bf8b
coouir <166575866+coouir@users.noreply.github.com>
2024-12-20 20:49:28 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241220/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while (1) {
+        int num;
+        cin >> num;
+
+        if (num == 0) {
+            break;
+        }
+        cout << num << endl;
+    }
+    return 0;
+}
