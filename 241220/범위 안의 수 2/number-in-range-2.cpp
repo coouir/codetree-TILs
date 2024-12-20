8a4669a9d44928f858782508248574cc0cc5db15
coouir <166575866+coouir@users.noreply.github.com>
2024-12-20 19:26:18 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241220/범위 안의 수 2/number-in-range-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, sum_val=0, cnt=0;
+    for (int i=0; i<10; i++) {
+        cin >> n;
+        if (0<=n && n<=200) {
+            sum_val += n;
+            cnt++;
+        }
+    }
+
+    cout << fixed;
+    cout.precision(1);
+    cout << sum_val << ' ' << (double)sum_val / cnt;
+    return 0;
+}
