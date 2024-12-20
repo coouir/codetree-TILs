7adb65fa210be825dfbd97571253c0f6f643ada2
coouir <166575866+coouir@users.noreply.github.com>
2024-12-20 18:58:32 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241220/홀수만 더하기/add-only-odd-numbers.cpp
+++ 241220/홀수만 더하기/add-only-odd-numbers.cpp
@@ -6,7 +6,7 @@ int main() {
     int n, num, sum_val = 0;
     cin >> n;
 
-    for (int i=0; i<=n; i++) {
+    for (int i=0; i<n; i++) {
         cin >> num;
 
         if (num%2 == 1 && num%3 == 0) {
