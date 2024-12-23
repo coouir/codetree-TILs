2b6bd13186156cf68582c2a74913e0caedfd6b0d
coouir <166575866+coouir@users.noreply.github.com>
2024-12-23 21:08:01 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241223/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10], sum_val=0, cnt=0;
+    for (int i=0; i<10; i++) {
+        cin >> arr[i];
+
+        if (arr[i] == 0) {
+            break;
+        }
+        sum_val += arr[i];
+        cnt++;
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << sum_val << ' ' << (double)sum_val/cnt;
+    return 0;
+}
