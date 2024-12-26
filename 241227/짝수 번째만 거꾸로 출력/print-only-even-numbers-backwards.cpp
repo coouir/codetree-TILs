edacd1a36074cbe5b563222b0ba90ada13beb978
coouir <166575866+coouir@users.noreply.github.com>
2024-12-27 03:50:26 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241227/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    for (int i=0; i<str.length(); i++) {
+        if ((str.length()-1-i)%2 == 1) cout << str[str.length()-1-i];
+    }
+    return 0;
+}
