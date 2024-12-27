89db767d1bafcfc3264c256ff5bd1ca5e8cae6e3
coouir <166575866+coouir@users.noreply.github.com>
2024-12-27 12:05:27 +0900

    Title: 첫 번째와 두 번째 교환, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241227/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    char c1 = str[0], c2 = str[1];
+    for (int i=0; i<str.length(); i++) {
+        if (str[i] == c1) {
+            str[i] = c2;
+        } else if (str[i] == c2) {
+            str[i] = c1;
+        }
+    }
+    cout << str;
+    return 0;
+}
