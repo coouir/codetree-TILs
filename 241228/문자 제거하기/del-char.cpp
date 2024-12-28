b8ed64236afc529104c7c7a2778b4a7e7b663d94
coouir <166575866+coouir@users.noreply.github.com>
2024-12-28 21:57:08 +0900

    Title: 문자 제거하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241228/문자 제거하기/del-char.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    int arr[100];
+    for (int i=0; i<str.length()-1; i++) {
+        // int num;
+        // cin >> num;
+        // str[num-1] = '.';
+        int num;
+        cin >> num;
+        if (num > str.length()) {
+            for (int j=0; j<str.length(); j++) {
+                if (str[str.length()-1-j] != '.') {
+                    str[str.length()-1-j] = '.';
+                    break;
+                }
+            }
+        } else {
+            str[num] = '.';
+        }
+        for (int j=0; j<str.length(); j++) {
+            if (str[j] != '.') {
+                cout << str[j];
+            }
+        }
+    }
+
+    cout << str;
+}
