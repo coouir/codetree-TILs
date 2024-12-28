8d164db077d85db1c9f1e0e7bd440c1d5e97a34a
coouir <166575866+coouir@users.noreply.github.com>
2024-12-29 00:51:34 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241229/Run Length 인코딩/run-length-encoding.cpp
+++ 241229/Run Length 인코딩/run-length-encoding.cpp
@@ -4,34 +4,23 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    string str, result="";
-    cin >> str;
-    for (int i=0; i<str.length(); i++) {
-        int cnt = 1;
-        for (int j=i+1; j<str.length(); j++) {
-            if (str[i] == str[j]) {
-                cnt++;
-            } else {
-                break;
-            }
-        }
-        if (i == 0) {
-            result += (str[i]+to_string(cnt));
+    string A, result = "";
+    cin >> A;
+
+    char c = A[0];
+    int cnt = 1;
+    for (int i=1; i<A.length(); i++) {
+        if (A[i] == c) {
+            cnt++;
         } else {
-            bool check = false;
-            for (int j=0; j<result.length(); j++) {
-                if (isalpha(result[result.length()-1-j])) {
-                    if (result[result.length()-1-j] != str[i]) {
-                        check = true;
-                    }
-                    break;
-                }
-            }
-            if (check) {
-                result += (str[i]+to_string(cnt));
-            }
+            result += c;
+            result += to_string(cnt);
+            c = A[i];
+            cnt = 1;
         }
     }
+    result += c;
+    result += to_string(cnt);
     cout << result.length() << endl;
     cout << result;
     return 0;
