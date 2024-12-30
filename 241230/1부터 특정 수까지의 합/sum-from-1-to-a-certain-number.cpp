d903df9028467b44c900fda5138baeaa003232f1
coouir <166575866+coouir@users.noreply.github.com>
2024-12-30 12:04:42 +0900

    Title: 1부터 특정 수까지의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241230/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
+++ 241230/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int F(int N) {
-    return N/2*(N+1)/10;
+    return (N+1)*N/2;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int N;
     cin >> N;
-    cout << F(N);
+    cout << F(N)/10;
     return 0;
 }
