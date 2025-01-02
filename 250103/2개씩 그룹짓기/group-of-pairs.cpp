ef39fd374635dedb12c584feb3a4ab15e4afaae8
coouir <166575866+coouir@users.noreply.github.com>
2025-01-03 00:42:01 +0900

    Title: 2개씩 그룹짓기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250103/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, arr[1000];
+    cin >> n;
+    for (int i=0; i<2*n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+2*n);
+    int Max = 0;
+    for (int i=0; i<n; i++) {
+        if (arr[i] + arr[2*n-1-i] > Max) {
+            Max = arr[i] + arr[2*n-1-i];
+        }
+    }
+    cout << Max;
+    return 0;
+}
