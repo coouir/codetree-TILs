843598e714d45e8163058797ce3ee4d37b7d8bd3
coouir <166575866+coouir@users.noreply.github.com>
2025-01-05 00:40:42 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250105/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 250105/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -3,26 +3,31 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int n, arr[2001]={}, p=1000;
+    int n, x1[100], x2[100], arr[2001]={};
     cin >> n;
+
+    int p = 0;
     for (int i=0; i<n; i++) {
         int x; char c;
         cin >> x >> c;
         if (c == 'L') {
-            for (int j=0; j<x; j++) {
-                arr[--p]++;
-            }
+            x1[i] = p-x + 1000;
+            x2[i] = p + 1000;
+            p -= x;
         } else {
-            for (int j=0; j<x; j++) {
-                arr[p++]++;
-            }
+            x1[i] = p + 1000;
+            x2[i] = p+x + 1000;
+            p += x;
+        }
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=x1[i]; j<x2[i]; j++) {
+            arr[j]++;
         }
     }
     int cnt = 0;
     for (int i=0; i<2001; i++) {
-        if (arr[i] >= 2) {
-            cnt++;
-        }
+        if (arr[i] >= 2) cnt++;
     }
     cout << cnt;
     return 0;
