54aaf4d8cc020213690de92ef602ad0b3b2d7a79
coouir <166575866+coouir@users.noreply.github.com>
2025-01-05 00:25:02 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, arr[2001]={}, p=1000;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        int x; char c;
+        cin >> x >> c;
+        if (c == 'L') {
+            if (x == 1) {
+                arr[--p]++;
+            } else {
+                for (int j=0; j<x; j++) {
+                    arr[--p]++;
+                }
+            }
+        } else {
+            if (x == 1) {
+                arr[p++]++;
+            } else {
+                for (int j=0; j<x; j++) {
+                    arr[p++]++;
+                }
+            }
+        }
+    }
+    int cnt = 0;
+    for (int i=0; i<2001; i++) {
+        if (arr[i] >= 2) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
