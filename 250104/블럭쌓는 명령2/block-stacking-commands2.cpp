ef18ac6322be53e1473a05a3ceeab4243344545a
coouir <166575866+coouir@users.noreply.github.com>
2025-01-04 21:09:42 +0900

    Title: 블럭쌓는 명령2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250104/블럭쌓는 명령2/block-stacking-commands2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[101]={}, N, K;
+    cin >> N >> K;
+    for (int i=0; i<K; i++) {
+        int a, b;
+        cin >> a >> b;
+        for (; a<=b; a++) {
+            arr[a]++;
+        }
+    }
+    int M = 0;
+    for (int i=0; i<N; i++) {
+        if (M < arr[i]) M = arr[i];
+    }
+    cout << M;
+    return 0;
+}
