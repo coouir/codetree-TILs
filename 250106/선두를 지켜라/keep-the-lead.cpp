42ac44d84193b0f8835dbe31d71b882b45c5e97b
coouir <166575866+coouir@users.noreply.github.com>
2025-01-06 20:47:15 +0900

    Title: 선두를 지켜라, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250106/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N, M, arrA[1001]={}, arrB[1001]={};
+    cin >> N >> M;
+    
+    int time_a = 1;
+    for (int i=0; i<N; i++) {
+        int v, t;
+        cin >> v >> t;
+        while (t--) {
+            arrA[time_a] = arrA[time_a-1]+v;
+            time_a++;
+        }
+    }
+    int time_b = 1;
+    for (int i=0; i<M; i++) {
+        int v, t;
+        cin >> v >> t;
+        while (t--) {
+            arrB[time_b] = arrB[time_b-1]+v;
+            time_b++;
+        }
+    }
+    int H = 0; // A: 2, B: 1
+    int cnt = 0;
+    for (int i=1; i<time_a; i++) {
+        if (H == 0) {
+            if (arrA[i] > arrB[i]) H = 2;
+            else if (arrA[i] < arrB[i]) H = 1;
+            continue;
+        }
+        if (H == 2 && arrA[i] < arrB[i]) {
+            H = 1;
+            cnt++;
+        } else if (H == 1 && arrA[i] > arrB[i]) {
+            H = 2;
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
