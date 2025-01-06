2aea4ce4bb517c27cf566871a7aa0253048ea658
coouir <166575866+coouir@users.noreply.github.com>
2025-01-06 20:52:29 +0900

    Title: 선두를 지켜라, Time: 13ms, Memory: 8MB, Status: Passed - CodeTree

--- 250106/선두를 지켜라/keep-the-lead.cpp
+++ 250106/선두를 지켜라/keep-the-lead.cpp
@@ -27,17 +27,12 @@ int main() {
     int H = 0; // A: 2, B: 1
     int cnt = 0;
     for (int i=1; i<time_a; i++) {
-        if (H == 0) {
-            if (arrA[i] > arrB[i]) H = 2;
-            else if (arrA[i] < arrB[i]) H = 1;
-            continue;
-        }
-        if (H == 2 && arrA[i] < arrB[i]) {
-            H = 1;
-            cnt++;
-        } else if (H == 1 && arrA[i] > arrB[i]) {
+        if (arrA[i] > arrB[i]) {
+            if (H == 1) cnt++;
             H = 2;
-            cnt++;
+        } else if (arrA[i] < arrB[i]) {
+            if (H == 2) cnt++;
+            H = 1;
         }
     }
     cout << cnt;
