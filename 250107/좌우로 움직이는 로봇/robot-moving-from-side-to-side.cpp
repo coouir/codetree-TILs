2c487a95b75e734768e7df8a3c4cfefb75a1f4ce
coouir <166575866+coouir@users.noreply.github.com>
2025-01-07 13:20:18 +0900

    Title: 좌우로 움직이는 로봇, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250107/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m, arrA[20]={0}, arrB[20]={0}; // 수정해야 함
+    cin >> n >> m;
+    int time_a = 1;
+    for (int i=0; i<n; i++) {
+        int t; char d;
+        cin >> t >> d;
+        if (d == 'L') {
+            while(t--) {
+                arrA[time_a] = arrA[time_a-1] - 1;
+                time_a++;
+            }
+        } else {
+            while(t--) {
+                arrA[time_a] = arrA[time_a-1] + 1;
+                time_a++;
+            }
+        }
+    }
+    int time_b = 1;
+    for (int i=0; i<m; i++) {
+        int t; char d;
+        cin >> t >> d;
+        if (d == 'L') {
+            while(t--) {
+                arrB[time_b] = arrB[time_b-1] - 1;
+                time_b++;
+            }
+        } else {
+            while(t--) {
+                arrB[time_b] = arrB[time_b-1] + 1;
+                time_b++;
+            }
+        }
+    }
+
+    if (time_a < time_b) {
+        for (int i=time_a; i<time_b; i++) {
+            arrA[i] = arrA[i-1];
+        }
+    } else if (time_b < time_a) {
+        for (int i=time_b; i<time_a; i++) {
+            arrB[i] = arrB[i-1];
+        }
+    }
+
+    int cnt = 0;
+    for (int i=1; i<max(time_a, time_b); i++) {
+        if (arrA[i] == arrB[i] && arrB[i-1] != arrA[i-1]) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
