f39b6a8b3269a527046be80ce4ecaae2e30af25a
coouir <166575866+coouir@users.noreply.github.com>
2025-01-09 12:24:14 +0900

    Title: 되돌아오기 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250109/되돌아오기 2/come-back-2.cpp
+++ 250109/되돌아오기 2/come-back-2.cpp
@@ -11,14 +11,14 @@ int main() {
     cin >> str;
     for (int i=0; i<str.length(); i++) {
         if (str[i] == 'L') {
-            dir_num = (dir_num+3)%4;
-        } else if (str[i] == 'R') {
             dir_num = (dir_num+1)%4;
+        } else if (str[i] == 'R') {
+            dir_num = (dir_num+3)%4;
         } else {
             x += dx[dir_num];
             y += dy[dir_num];
+            if (x == 0 && y == 0) ans = (i+1);
         }
-        if (x == 0 && y == 0) ans = (i+1);
     }
     cout << ans;
     return 0;
