77ada6e86a70cf2203ad2863219b59428ff24211
coouir <166575866+coouir@users.noreply.github.com>
2025-01-13 21:31:06 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250113/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, arr[100];
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    int max_val = 0;
+    for (int i=0; i<n; i++) {
+        int m = 0;
+        for (int j=0; j<=i-2; j++) {
+            if (m < arr[j]) m = arr[j];
+        }
+        for (int k=i+2; k<n; k++) {
+            if (m < arr[k]) m = arr[k];
+        }
+        if (max_val < arr[i] + m) max_val = arr[i] + m;
+    }
+    cout << max_val;
+    return 0;
+}
