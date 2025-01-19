f0f0e73df64303521cfe79f50be2372402f43754
coouir <166575866+coouir@users.noreply.github.com>
2025-01-19 23:27:46 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250119/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[20][20];
+
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int max_val = 0;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N-2; j++) {
+            for (int k=0; k<N; k++) {
+                for (int l=0; l<N-2; l++) {
+                    if (i == k && j == l) continue;
+                    max_val = max(max_val, arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2]);
+
+                }
+            }
+        }
+    }
+    cout << max_val;
+
+    return 0;
+}
