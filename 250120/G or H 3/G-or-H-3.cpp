cac744a34ece2cf2ef52cc2a34d3b2c23dc50b05
coouir <166575866+coouir@users.noreply.github.com>
2025-01-20 14:07:12 +0900

    Title: G or H 3, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250120/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[101]={}, L = 0;
+
+    int N, K;
+    cin >> N >> K;
+    for (int i=0; i<N; i++) {
+        int x; char c;
+        cin >> x >> c;
+        if (c == 'G') arr[x] = 1;
+        else arr[x] = 2;
+        L = max(L, x);
+    }
+
+    int max_val = 0;
+    for (int i=1; i<L-K+1; i++) {
+        int sum_val = 0;
+        for (int j=i; j<=i+K; j++) 
+            sum_val += arr[j];
+        max_val = max(max_val, sum_val);
+    }
+    cout << max_val;
+
+    return 0;
+}
