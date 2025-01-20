3fdccfc30f6a2cbe22c2d7f5f4f072113ae49172
coouir <166575866+coouir@users.noreply.github.com>
2025-01-20 14:14:28 +0900

    Title: G or H 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250120/G or H 3/G-or-H-3.cpp
+++ 250120/G or H 3/G-or-H-3.cpp
@@ -4,23 +4,32 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int arr[10001]={}, L = 0;
+    int arr[10001]={};
+    int S = 1 << 30;
+    int L = -(1 << 30);
 
     int N, K;
     cin >> N >> K;
     for (int i=0; i<N; i++) {
         int x; char c;
         cin >> x >> c;
+
         if (c == 'G') arr[x] = 1;
         else arr[x] = 2;
+        
+        S = min(S, x);
         L = max(L, x);
     }
+    cout << S << ' ' << L << endl;
 
     int max_val = 0;
-    for (int i=1; i<L-K+1; i++) {
+    for (int i=S; i<L-K+1; i++) {
         int sum_val = 0;
-        for (int j=i; j<=i+K; j++) 
+
+        for (int j=i; j<=i+K; j++) {
             sum_val += arr[j];
+        }
+        
         max_val = max(max_val, sum_val);
     }
     cout << max_val;
