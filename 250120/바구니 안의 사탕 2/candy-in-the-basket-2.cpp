61828866b2842f2a7de9353ecc29725f98b358f8
coouir <166575866+coouir@users.noreply.github.com>
2025-01-20 15:49:26 +0900

    Title: 바구니 안의 사탕 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250120/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[101]={};
+
+    int N, K;
+    cin >> N >> K;
+    for (int i=0; i<N; i++) {
+        int c, x;
+        cin >> c >> x;
+        arr[x] += c;
+    }
+
+    int max_val = 0;
+    for (int i=0; i<101 -2*K; i++) {
+        int sum_val = 0;
+        for (int j=i; j<=i+2*K; j++) {
+            sum_val += arr[j];
+        }
+        max_val = max(max_val, sum_val);
+    }
+    cout << max_val;
+    
+    return 0;
+}
