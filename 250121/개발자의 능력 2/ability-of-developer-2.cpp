48772a39ab7e511aa9f7e6fe76373873a540cbd0
coouir <166575866+coouir@users.noreply.github.com>
2025-01-21 20:50:15 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250121/개발자의 능력 2/ability-of-developer-2.cpp
+++ 250121/개발자의 능력 2/ability-of-developer-2.cpp
@@ -5,14 +5,28 @@ using namespace std;
 int main() {
     // Please write your code here.
     int arr[6];
+    
+    int sum_val = 0;
     for (int i=0; i<6; i++) {
         cin >> arr[i];
+        sum_val += arr[i];
     }
 
-    sort(arr, arr+6);
-
-    int max_val = max(arr[5]+arr[0], max(arr[4]+arr[1], arr[3]+arr[2]));
-    int min_val = min(arr[5]+arr[0], min(arr[4]+arr[1], arr[3]+arr[2]));
-    cout << max_val - min_val;
+    int min_val = 1 << 30;
+    for (int i=0; i<6; i++) {
+        for (int j=i+1; j<6; j++) {
+            for (int k=0; k<6; k++) {
+                for (int l=k+1; l<6; l++) {
+                    if (i == k || i == l || j == k || j == l) continue;
+                    int team1 = arr[i] + arr[j];
+                    int team2 = arr[k] + arr[l];
+                    int team3 = sum_val - team1 - team2;
+                    min_val = min(min_val, max(team1, max(team2, team3)) - min(team1, min(team2, team3)));
+                }
+            }
+        }
+    }
+    cout << min_val;
+    
     return 0;
 }
