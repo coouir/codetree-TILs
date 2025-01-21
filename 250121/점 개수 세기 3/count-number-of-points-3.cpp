4a0afdcc60a3708a4bf9ce1a6097446e9a2c0ed7
coouir <166575866+coouir@users.noreply.github.com>
2025-01-21 13:30:17 +0900

    Title: 점 개수 세기 3, Time: 71ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 250121/점 개수 세기 3/count-number-of-points-3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[100001] = {};
+
+    int n, q;
+    scanf("%d %d", &n, &q);
+    for (int i=0; i<n; i++)  
+        scanf("%d", &arr[i]);
+
+    sort(arr, arr+n);
+
+    map<int, int> mapper;
+    for (int i=0; i<n; i++) {
+        mapper[arr[i]] = i+1;
+        arr[i] = mapper[arr[i]];
+    }
+    
+    int arr2[100001] = {};
+    for (int i=0; i<n; i++) {
+        arr2[arr[i]] = 1;
+    }
+    
+    int prefix[100001] = {};
+    for (int i=1; i<=100001; i++) {
+        prefix[i] = arr2[i] + prefix[i-1];
+    }
+
+    for (int i=0; i<q; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        a = mapper[a];
+        b = mapper[b];
+        cout << prefix[b] - prefix[a-1] << '\n';
+    }
+    return 0;
+}
