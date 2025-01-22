a01141e322a96aa3235d9875b24b96d56704a357
coouir <166575866+coouir@users.noreply.github.com>
2025-01-22 21:22:46 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250122/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 250122/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -3,43 +3,28 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int arr[100][2];
+    int x1[100], x2[100];
 
     int N;
     cin >> N;
     for (int i=0; i<N; i++) 
-        cin >> arr[i][0] >> arr[i][1];
+        cin >> x1[i] >> x2[i];
     
     int cnt = 0;
     for (int i=0; i<N; i++) {
-        int check = true;
+
+        bool check = true;
         for (int j=0; j<N; j++) {
             if (i == j) continue;
 
-            int x1 = arr[i][0];
-            int x2 = arr[i][1];
-            int x3 = arr[j][0];
-            int x4 = arr[j][1];
-
-            if (x1 <= x2) {
-                if (x3 < x1 && x4 < x2) {
-                    continue;
-                } else if (x1 < x3 && x2 < x4) {
-                    continue;
-                }
-            } else {
-                if (x4 < x2 && x3 < x1) {
-                    continue;
-                } else if (x2 < x4 && x1 < x3) {
-                    continue;
-                }
+            if ((x1[j] < x1[i] && x2[i] < x2[j]) || (x1[i] < x1[j] && x2[j] < x2[i])) {
+                check = false;
+                break;
             }
-            
-            check = false;
-            break;
         }
+
         if (check) cnt++;
-    }
+    }    
     cout << cnt;
     
     return 0;
