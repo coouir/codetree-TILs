db7544e728c6d72ebc13105a0bb295933304cfd0
coouir <166575866+coouir@users.noreply.github.com>
2025-01-24 20:42:21 +0900

    Title: 흥미로운 숫자 2, Time: 25ms, Memory: 0MB, Status: Passed - CodeTree

--- 250124/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250124/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -6,16 +6,17 @@ bool F(int n) {
     int arr[10] = {};
 
     string str = to_string(n);
-    for (int i=0; i<str.length(); i++) {
+    for (int i=0; i<str.length(); i++) 
         arr[str[i]-'0']++;
-    }  
 
     int cnt = 0;
-    for (int i=0; i<10; i++) {
+    bool check1 = false;
+    for (int i=0; i<10; i++) { 
         if (arr[i] != 0) cnt++;
+        if (arr[i] == 1) check1 = true;
     }
 
-    if (cnt == 2) return true;
+    if (cnt == 2 && check1) return true;
     return false;
 }
 
