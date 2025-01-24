df22d415892b4ec9232c9e7f5a2c195b69d86f98
coouir <166575866+coouir@users.noreply.github.com>
2025-01-24 14:07:49 +0900

    Title: 개발자의 순위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250124/개발자의 순위/developers-rank.cpp
+++ 250124/개발자의 순위/developers-rank.cpp
@@ -18,13 +18,26 @@ int main() {
     for (int i=0; i<K; i++) { // 경기
         for (int j=0; j<N; j++) {
             for (int k=j+1; k<N; k++) {
-                mathch_result[stoi(to_string(arr[i][j]) + to_string(arr[i][k]))]++;
+                string str1, str2;
+                if (arr[i][j] < 10) {
+                    str1 = "0" + to_string(arr[i][j]);
+                } else {
+                    str1 = to_string(arr[i][j]);
+                }
+
+                if (arr[i][k] < 10) {
+                    str2 = "0" + to_string(arr[i][k]);
+                } else {
+                    str2 = to_string(arr[i][k]);
+                }
+
+                mathch_result[stoi(str1+str2)]++;
             }
         }
     }
     
     int cnt = 0;
-    for (int i=0; i<2020; i++) {
+    for (int i=0; i<3000; i++) {
         if (mathch_result[i] == K) cnt++;
     }
     cout << cnt;
