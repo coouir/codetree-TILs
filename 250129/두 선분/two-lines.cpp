a8f96b0afe5b9ebcaad86896db0715c602314fb0
coouir <166575866+coouir@users.noreply.github.com>
2025-01-29 12:27:55 +0900

    Title: 두 선분, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250129/두 선분/two-lines.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x1, x2, x3, x4;
+    cin >> x1 >> x2 >> x3 >> x4;
+
+    if ((x1 <= x3 && x3 <= x2 && x2 <= x4)
+        || (x3 <= x1 && x1 <= x4 && x4 <= x2)
+        || (x3 <= x1 && x1 <= x2 && x2 <= x4) 
+        || (x1 <= x3 && x3 <= x4 && x4 <= x2)) {
+        cout << "intersecting";
+    } else {
+        cout << "nonintersecting";
+    }
+    return 0;
+}
