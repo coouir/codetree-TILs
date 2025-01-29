8426c17560ce2a62d7930edcea0a28276a721491
coouir <166575866+coouir@users.noreply.github.com>
2025-01-29 14:13:49 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250129/이상한 폭탄 3/strange-bomb-3.cpp
+++ 250129/이상한 폭탄 3/strange-bomb-3.cpp
@@ -19,18 +19,29 @@ int main() {
             if (arr[i] == n) arrSat[index++] = i;
         }
         
+        // for (int i=0; i<index; i++) {
+        //     cout << arrSat[i] << ' ';
+        // }
+        // cout << endl;
+
         int cnt = 0;
         for (int i=0; i<index-1; i++) {
-            if (arrSat[i] - arrSat[i-1] > K) break;
-            cnt++;
+            if (arrSat[i+1] - arrSat[i] <= K) {
+                cnt++;
+            }
         }
         if (cnt != 0) {
             if (max_val <= cnt) {
                 max_val = cnt;
                 max_n = n;
             }
+            //cout << n << "  " << cnt+1 << endl;
         } 
+        // else {
+        //     cout << n << "  " << 0 << endl;
+        // }
     }
+    //cout << endl;
     cout << max_n;
     
     return 0;
