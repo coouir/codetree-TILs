9f5ef8e1816c707556a38a0d3ad23eb11e078d3c
coouir <166575866+coouir@users.noreply.github.com>
2025-01-30 17:56:58 +0900

    Title: 코딩톡, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 250130/코딩톡/coding-talk.cpp
+++ 250130/코딩톡/coding-talk.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int member[26] = {};
+    int member[100][26] = {};
 
     char c[100];
     int u[100];
@@ -11,17 +11,50 @@ int main() {
     int n, m, p;
     cin >> n >> m >> p;
 
+    p--;
+
     for (int i=0; i<m; i++) 
         cin >> c[i] >> u[i];
     
-    if (u[p-1] == 0) return 0;
+    for (int i=0; i<=p; i++) {
+        if (u[i] == 0) {
+            for (int j=0; j<n; j++) member[i][j] = 1;  
+        } else {
+            for (int j=i; j<m; j++) member[i][c[j]-65] = 1;  
+        }
+    }
     
-    for (int i=p-1; i<m; i++) {
-        member[c[i] - 65] = 1;
+    // for (int i=0; i<m; i++) {
+    //     for (int j=0; j<n; j++) {
+    //         cout << member[i][j] << ' ';
+    //     }
+    //     cout << endl;
+    // }
+
+    if (p != 0) {
+        int index = p;
+        for (int i=p-1; i>=0; i--) {
+            if (u[i] == u[p]) index = i;
+            else break;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (member[index][i] == 0) cout << (char) (i+65) << ' ';
+        }
+    } else {
+        for (int i=0; i<n; i++) {
+            if (member[0][i] == 0) cout << (char) (i+65) << ' ';
+        }
     }
 
-    for (int i=0; i<n; i++) {
-        if (member[i] == 0) cout << (char) (i+65) << ' ';
-    }
     return 0;
 }
+
+// A B C D E F
+
+// D 0 A B C D E F
+// C 1 C B A F         D E 
+// B 2 B A F           C D E
+// B 2 B A F           C D E
+// A 2 A F             B C D E
+// F 4 F               A B C D E F
