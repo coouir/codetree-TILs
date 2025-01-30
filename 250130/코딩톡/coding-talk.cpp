ea41b855d6e011978bcb83dfc6d75c78b3555806
coouir <166575866+coouir@users.noreply.github.com>
2025-01-30 17:38:58 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250130/코딩톡/coding-talk.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int member[26] = {};
+
+    char c[100];
+    int u[100];
+
+    int n, m, p;
+    cin >> n >> m >> p;
+
+    for (int i=0; i<m; i++) 
+        cin >> c[i] >> u[i];
+    
+    for (int i=p-1; i<m; i++) {
+        member[c[i] - 65] = 1;
+    }
+
+    for (int i=0; i<n; i++) {
+        if (member[i] == 0) cout << (char) (i+65) << ' ';
+    }
+    return 0;
+}
