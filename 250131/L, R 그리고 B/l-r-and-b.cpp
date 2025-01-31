28ebf1f9b9c67123c07be6e11b1df13a0051bd6e
coouir <166575866+coouir@users.noreply.github.com>
2025-01-31 21:31:49 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250131/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+char arr[11][11];
+
+int main() {
+    // Please write your code here.
+    for (int i=1; i<=10; i++) 
+        for (int j=1; j<=10; j++) 
+            cin >> arr[i][j];
+        
+    int xL, yL, xB, yB;
+    for (int i=1; i<=10; i++) {
+        for (int j=1; j<=10; j++) {
+            if (arr[i][j] == 'L') {
+                xL = i;
+                yL = j;
+            } else if (arr[i][j] == 'B') {
+                xB = i;
+                yB = j;
+            }
+        }
+    }
+
+    cout << abs(xL - xB) + abs(yL - yB) - 1;
+    return 0;
+}
