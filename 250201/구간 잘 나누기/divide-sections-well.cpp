e47ede6d8a038bae0ca37cdb694ba0ee1fe11938
coouir <166575866+coouir@users.noreply.github.com>
2025-02-01 20:10:22 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250201/구간 잘 나누기/divide-sections-well.cpp
+++ 250201/구간 잘 나누기/divide-sections-well.cpp
@@ -13,7 +13,14 @@ int main() {
     for (int max_i=1; max_i<=10000; max_i++) {
         int cnt = 0;
         int sum_val = 0;
+
+        bool check = true;
         for (int i=0; i<n; i++) {
+            if (arr[i] > max_i) {
+                check = false;
+                break;
+            }
+
             if (sum_val + arr[i] > max_i) {
                 sum_val = arr[i];
                 cnt++;
@@ -21,7 +28,10 @@ int main() {
                 sum_val += arr[i];
             }
         }
-        if (m-1 == cnt) {
+        if (check == false) continue;
+        
+        //cout << max_i << "  " << cnt << endl;
+        if (m-1 >= cnt) {
             cout << max_i;
             break;
         }
