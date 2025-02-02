95384e063b9c60dcd5bc9f7d67d20d864eb85f30
coouir <166575866+coouir@users.noreply.github.com>
2025-02-02 11:18:25 +0900

    Title: 두 점으로 만드는 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/두 점으로 만드는 직사각형/rectangle-made-of-two-points.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x1, y1, x2, y2;
+    int a1, b1, a2, b2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> a1 >> b1 >> a2 >> b2;
+
+    x1 = min(x1, a1);
+    y1 = min(y1, b1);
+    x2 = max(x2, a2);
+    y2 = max(y2, b2);
+
+    cout << (x2-x1) * (y2-y1);
+    return 0;
+}
