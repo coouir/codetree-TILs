c605ab9439f32da0fb5772ff3aa756751c1f8ed1
coouir <166575866+coouir@users.noreply.github.com>
2025-02-04 15:50:27 +0900

    Title: 2개씩 그룹짓기 2, Time: 90ms, Memory: 1MB, Status: Passed - CodeTree

--- 250204/2개씩 그룹짓기 2/group-of-pairs-2.cpp
+++ 250204/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    long long arr[100000];
+    int arr[220000];
     
     int n;
     cin >> n;
@@ -13,7 +13,7 @@ int main() {
     
     sort(arr, arr+2*n);
 
-    long long min_val = 1 << 30;
+    int min_val = 1 << 30;
     for (int i=0; i<n; i++) {
         min_val = min(min_val, arr[i+n] - arr[i]);
     }
