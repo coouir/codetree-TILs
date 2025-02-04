7430e38625370f72698f15b34f98f4400c0796cc
coouir <166575866+coouir@users.noreply.github.com>
2025-02-04 17:31:07 +0900

    Title: 최대 H 점수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250204/최대 H 점수 2/maximum-h-score-2.cpp
+++ 250204/최대 H 점수 2/maximum-h-score-2.cpp
@@ -1,46 +1,60 @@
 #include <iostream>
 #include <algorithm>
-
-#define MAX_N 1000
-
 using namespace std;
 
-int n, l;
-int a[MAX_N];
-
 int main() {
-    // 입력:
-    cin >> n >> l;
-    
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-    
-    // 모든 답을 일일히 가정해 봅니다.
-    int ans = 0;
-    for(int i = 1; i <= n; i++) {
-        // 정답이 i일 때 가능한지 판단합니다.
+    // Please write your code here.
+    int arr[100];
 
-        // i - 1인 값은 최대 l개까지 i로 올릴 수 있습니다.
-        // cnt : i이상인 숫자의 개수(i - 1인 숫자는 l개까지 카운트)
-        // cntl : 지금까지 1 증가시킨 숫자의 개수
+    int N, L;
+    cin >> N >> L;
+    for (int i=0; i<N; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr+N);
+    
+    int max_val = 0;
+    for (int h=1; h<=100; h++) {
         int cnt = 0;
-        int cntl = 0;
-
-        for(int j = 0; j < n; j++) {
-            if(a[j] >= i)
-                cnt++;
-            else if(a[j] == i - 1)
-                if(cntl < l) {
-                    cntl++;
-                    cnt++;
-                }
+        int index = 0;
+        for (int i=0; i<N; i++) {
+            if (arr[i] < h) index = i;
+            else cnt++;
+        }
+        int time = L;
+        while(time) {
+            if (arr[index--]+1 >= h) cnt++;
+            time--;
         }
 
-        if(cnt >= i)
-            ans = i;
+        if (cnt >= h) {
+            max_val = h;
+        }
     }
-
-    cout << ans;
+    cout << max_val;
 
     return 0;
 }
+
+
+
+// 4 1
+// 1   100   2   3
+
+// 1   2   3   100
+
+// 2   2   3   100
+
+// 1   3   3   100
+
+// 1   2   4   100
+
+
+
+
+
+// 1   2   3   4   5
+
+//     3    4    4
+// 서로 다른 2개
+
