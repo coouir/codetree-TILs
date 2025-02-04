2f394158e5f444ea439628234d3bd596f6ac157c
coouir <166575866+coouir@users.noreply.github.com>
2025-02-04 14:34:13 +0900

    Title: 기수 정렬 구현, Time: 36ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 250204/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[100000];
+    
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++) 
+        cin >> arr[i];
+    
+    for (int i=5; i>=0; i--) {
+        vector<int> arr_new[10];
+        for (int j=0; j<n; j++) {
+            int t = 5-i;
+
+            int num = arr[j];
+            for (int k=0; k<t; k++) {
+                num /= 10;
+            }
+            arr_new[num%10].push_back(arr[j]);
+        }
+
+        int index = 0;
+        int store_arr[100000];
+        for (int j=0; j<10; j++) {
+            for (int k=0; k<arr_new[j].size(); k++) {
+                store_arr[index++] = arr_new[j][k];
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            arr[i] = store_arr[i];
+        }
+    }
+
+    for (int i=0; i<n; i++) 
+        cout << arr[i] << ' ';
+    return 0;
+}
