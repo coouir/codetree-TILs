0ce7a23e703fe6e0ba54cf3143bcbda312ab71b9
coouir <166575866+coouir@users.noreply.github.com>
2025-02-04 15:45:08 +0900

    Title: 2개씩 그룹짓기 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250204/2개씩 그룹짓기 2/group-of-pairs-2.cpp
+++ 250204/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
 #include <algorithm>
-#include <cstdlib>
 using namespace std;
 
 int main() {
     // Please write your code here.
     int arr[100000];
-
+    
     int n;
     cin >> n;
-    for (int i=0; i<n; i++)
-      cin >> arr[i];
+    for (int i=0; i<2*n; i++) 
+        cin >> arr[i];
     
-    int max_val = 0;
-    for (int i=0; i<n; i++) {
-      for (int j=i+1; j<n; j++) {
-        int check = true;
-        for (int k=0; k<n; k++) {
-          for (int l=k+1; l<n; l++) {
-            if (i == k || i == l || j == k || j == l) continue;
-            if (abs(arr[i]-arr[j]) > abs(arr[k]-arr[l])) {
-              check = false;
-              break;
-            }
-          }
-        }
-        if (check) {
-          max_val = abs(arr[i]-arr[j]);
-        }
-      }
-    }
-    cout << max_val; 
+    sort(arr, arr+n);
+
+    int min_val = 1 << 30;
+    for (int i=0; i<n; i++) 
+        min_val = min(min_val, arr[i+n] - arr[i]);
+    
+    cout << min_val;
+    
     return 0;
 }
 
@@ -51,3 +39,4 @@ int main() {
 
 
 // 2, 15
+
