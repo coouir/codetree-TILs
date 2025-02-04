a35650dc35ed8d3ae4d6b2ab2628b7014e358b4b
coouir <166575866+coouir@users.noreply.github.com>
2025-02-04 11:16:49 +0900

    Title: 황금비율 토스트, Time: 72ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 250204/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    list <char> l;
+    list <char>::iterator it;
+
+    int n, m;
+    char c;
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+        cin >> c;
+        l.push_back(c);
+    }
+
+    it = l.end();
+    it--;
+    for (int i=0; i<m; i++) {
+        cin >> c;
+        if (c == 'L') {
+            if (it == l.begin()) continue;
+            it--;
+        } else if (c == 'R') {
+            if (it == l.end()) continue;
+            it++;
+        } else if (c == 'D') {
+            l.erase(it);
+        } else if (c == 'P') {
+            char ch;
+            cin >> ch;
+            it++;
+            l.insert(it, ch);
+        }
+    }
+
+    for (it = l.begin(); it != l.end(); it++) {
+        cout << *it;
+    }
+    return 0;
+}
