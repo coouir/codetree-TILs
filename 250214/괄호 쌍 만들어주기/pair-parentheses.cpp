f20b16aa854c891aa58bd76ec870efd899d38c31
coouir <166575866+coouir@users.noreply.github.com>
2025-02-14 20:19:09 +0900

    Title: 괄호 쌍 만들어주기, Time: 9ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 250214/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    int suffix[110000] = {};
+
+    for (int i=(int)str.size()-1; i>=0; i--) {
+        if (str[i] == ')' && str[i+1] == ')') {
+            suffix[i] = suffix[i+1] + 1;
+        } else {
+            suffix[i] = suffix[i+1];
+        }
+    }
+
+    int cnt = 0;
+    for (int i=0; i<(int)str.size()-1; i++) {
+        if (str[i] == '(' && str[i+1] == '(') {
+            cnt += suffix[i+2];
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
