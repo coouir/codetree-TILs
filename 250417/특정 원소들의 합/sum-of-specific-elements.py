c2dfc0e7fe5bcc744a6f5c31a009bdc7fc824041
coouir <166575866+coouir@users.noreply.github.com>
2025-04-17 18:19:23 +0900

    Title: 특정 원소들의 합, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250417/특정 원소들의 합/sum-of-specific-elements.py
+++ 250417/특정 원소들의 합/sum-of-specific-elements.py
@@ -3,7 +3,7 @@ arr = [list(map(int, input().split())) for i in range(4)]
 cnt = 0
 for i in range(4):
     for j in range(4):
-        if j>i: continue
+        if j>i: break
         cnt += arr[i][j]
 
 print(cnt)
