89ddc0629c691d5bf28c48c2456bd9410275161a
coouir <166575866+coouir@users.noreply.github.com>
2025-06-26 21:52:15 +0900

    Title: 되돌아오기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250626/되돌아오기/come-back.py
@@ -0,0 +1,26 @@
+x, y = 0, 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+mapper = {
+    'N' : 0,
+    'E' : 1,
+    'S' : 2,
+    'W' : 3 
+}
+
+cnt = 0
+
+N = int(input())
+for i in range(N):
+    d, dist = map(str, input().split())
+    dist = int(dist)
+    
+    for j in range(dist):
+        x += dx[mapper[d]]
+        y += dy[mapper[d]]
+
+        cnt += 1
+        if x == 0 and y == 0:
+            print(cnt)
+            exit()
