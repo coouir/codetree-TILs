86df594573c9f2c5dc7f10b68dc52d9255ecb17d
coouir <166575866+coouir@users.noreply.github.com>
2025-07-01 20:08:32 +0900

    Title: 정수 N개의 합 3, Time: 87ms, Memory: 21MB, Status: Passed - Codetree

--- 250701/정수 N개의 합 3/sum-of-n-integers-3.py
+++ 250701/정수 N개의 합 3/sum-of-n-integers-3.py
@@ -1,15 +1,15 @@
 N, K = map(int, input().split())
 
-arr = [[0] * (N + 1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
-
+arr = [[0 for _ in range(N+1)]] + [[0] + list(map(int, input().split())) for _ in range(N)]
 prefix = [[0 for j in range(N+1)] for i in range(N+1)]
+
 for i in range(1, N+1):
     for j in range(1, N+1):
         prefix[i][j] = arr[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
 
-M = 0
+answer = 0
 for i in range(K, N+1):
     for j in range(K, N+1):
-        M = max(M, prefix[i][j] - prefix[i-K][j] - prefix[i][j-K] + prefix[i-K][j-K])
+        answer = max(answer, prefix[i][j] - prefix[i-K][j] - prefix[i][j-K] + prefix[i-K][j-K])
 
-print(M)
+print(answer)
