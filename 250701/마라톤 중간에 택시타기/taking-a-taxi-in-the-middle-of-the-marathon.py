88b5e7797fdbfc5b7ea215c0e6b3e9e54a2edf3a
coouir <166575866+coouir@users.noreply.github.com>
2025-07-01 20:47:25 +0900

    Title: 마라톤 중간에 택시타기, Time: 235ms, Memory: 31MB, Status: Passed - Codetree

--- 250701/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
+++ 250701/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
@@ -1,16 +1,16 @@
 N = int(input())
-
-arr = [list(map(int, input().split())) for i in range(N)]
-prefix = [0 for i in range(N)]
-suffix = [0 for i in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
+prefix = [0 for _ in range(N)]
+suffix = [0 for _ in range(N)]
 
 for i in range(1, N):
-    prefix[i] = prefix[i-1] + abs(arr[i][0] - arr[i-1][0]) + abs(arr[i][1] - arr[i-1][1])
-for i in range(N-2, -1, -1):
-    suffix[i] = suffix[i+1] + abs(arr[i][0] - arr[i+1][0]) + abs(arr[i][1] - arr[i+1][1])
+    prefix[i] = abs(arr[i][0] - arr[i-1][0]) + abs(arr[i][1] - arr[i-1][1]) + prefix[i-1]
 
-m = 1<<60
+for i in range(0, N-1)[::-1]:
+    suffix[i] = abs(arr[i][0] - arr[i+1][0]) + abs(arr[i][1] - arr[i+1][1]) + suffix[i+1]
+
+answer = 1<<30
 for i in range(1, N-1):
-    m = min(m, prefix[i-1] + suffix[i+1] + abs(arr[i+1][0] - arr[i-1][0]) + abs(arr[i+1][1] - arr[i-1][1]))
+    answer = min(answer, prefix[i-1] + suffix[i+1] + abs(arr[i+1][0] - arr[i-1][0]) + abs(arr[i+1][1] - arr[i-1][1]))
 
-print(m)
+print(answer)
