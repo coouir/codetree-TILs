6cf43927bbc9d18af06a07dc441faa0cb3058945
coouir <166575866+coouir@users.noreply.github.com>
2025-07-02 14:16:43 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 312ms, Memory: 35MB, Status: Passed - Codetree

--- 250702/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 250702/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -5,11 +5,11 @@ prefix = [0 for _ in range(1000010)]
 
 I = list(map(int, input().split()))
 for i in range(N):
-    arr[I[i]] = 1
+    arr[I[i]+1] = 1
 
 for i in range(1, 1000010):
     prefix[i] = arr[i] + prefix[i-1]
 
 for q in range(Q):
     A, B = map(int, input().split())
-    print(prefix[B] - prefix[A-1])
+    print(prefix[B+1] - prefix[A])
